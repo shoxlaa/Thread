f14a4346ba882e5d5f22b4da02c2f78d90647818
Guli_tp48@itstep.edu.az <Guli_tp48@itstep.edu.az>
2022-07-06 21:28:41 +0400

    ff

--- Thread/Program.cs
+++ Thread/Program.cs
@@ -11,63 +11,86 @@
 
 //    }
 //}
+DateTime Begin = DateTime.Now;
+DateTime End = DateTime.Now;
+int iteration = 0;
+double Difference = 0;
 
-//Thread thread1 = new Thread(() =>
-//{
-
-//})
-//{
-//	IsBackground = true
-//}; 
-
-
-MyTimer timer = new(); 
-
-timer.Start();
-
-public class MyTimer
+Thread CountTime = new Thread(() =>
 {
-public DateTime Begin { get; set; } 
-public DateTime End { get; set; }
-public System.Timers.Timer Timer { get; set; }  
-public int Interval { get; set; }	= 500;
-public int Difference { get; private set; }
+    End = DateTime.Now;
 
-	public MyTimer()
-    {
-		Timer = new System.Timers.Timer(); 
-		
-	} 
-	public void Start()
-    {
-		Timer.Interval = Interval;
-		Timer.Elapsed += OnTimedEvent;
-		Timer.AutoReset = true;
-		Timer.Enabled = true;
-		Console.WriteLine("Press the Enter key to exit anytime... ");
-		Console.ReadLine();
+    Difference = (End - Begin).TotalMilliseconds;
+})
+{
+    IsBackground = true
+};
 
-		// Releasing Timer resources once done
-		Timer.Stop();
-		Timer.Dispose(); 
+Thread SpanValue = new Thread(() =>
+{
+    Console.WriteLine("Press Space");
+    Console.ReadKey();
+    Begin = DateTime.Now;
+})
+{
+    IsBackground = true
+};
 
-		Console.WriteLine(Difference);
-
-	}
-	private int iteration = 0;
-	 void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
-	{
-		// Code to be executed at the end of Timer
-		if (iteration == 0)
-		{
-			Begin = DateTime.Now; 
-			iteration++;
-		} 
-		End = DateTime.Now; 
-
-		Difference =  (End.Second-Begin.Second);
-
-	}
-}
+SpanValue.Start();
+CountTime.Start();
+SpanValue.Join();
+CountTime.Join();
+Console.WriteLine(Difference);
 
 
+
+
+//public class MyTimer
+//{
+//public DateTime Begin { get; set; } 
+//public DateTime End { get; set; }
+//public System.Timers.Timer Timer { get; set; }  
+//public int Interval { get; set; }	= 500;
+//public int Difference { get; private set; }
+
+//	public MyTimer()
+//    {
+//		Timer = new System.Timers.Timer(); 
+
+//	} 
+//	public void Start()
+//    {
+//		Timer.Interval = Interval;
+//		Timer.Elapsed += OnTimedEvent;
+//		Timer.AutoReset = true;
+//		Timer.Enabled = true;
+//		Console.WriteLine("Press the Enter key to exit anytime... ");
+//		Console.ReadLine();
+
+//		// Releasing Timer resources once done
+//		Timer.Stop();
+//		Timer.Dispose(); 
+
+//		Console.WriteLine(Difference);
+
+//	}
+//	private int iteration = 0;
+//	 void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+//	{
+//		// Code to be executed at the end of Timer
+//		if (iteration == 0)
+//		{
+//			Begin = DateTime.Now; 
+//			iteration++;
+//		} 
+//		End = DateTime.Now; 
+
+//		Difference =  (End.Second-Begin.Second);
+
+//	}
+//}
+
+
+//MyTimer timer = new();
+
+//timer.Start();
