d72380bb6a8d85846f79f6c4f92ee148c518e87e
shoxlaa <shahla.guliyeva02@gmail.com>
2022-07-06 16:32:45 +0400

    Добавьте файлы проекта.

---
+++ Thread/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading;
+
+Console.WriteLine("Hello, World!");
+
+ Timer _timer = null;
+// Create a Timer object that knows to call our TimerCallback
+// method once every 2000 milliseconds.
+_timer = new Timer(TimerCallback, null, 0, 2000);
+// Wait for the user to hit <Enter>
+Console.ReadLine();
+ static void TimerCallback(Object o)
+{
+    // Display the date/time when this method got called.
+    Console.WriteLine("In TimerCallback: " + DateTime.Now);
+}
+
+while (true)
+{
+    Console.WriteLine("Start? Yes Or No");
+    string? v = Console.ReadLine(); 
+    if (v == "Yes")
+    {
+
+    }
+}
+
+Thread thread1 = new Thread(() =>
+{
+	
+})
+{
+	IsBackground = true
+};
+
+
